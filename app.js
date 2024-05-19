48c7c2fb07d7f04ae8b585dbf468d88f83f00796
Rohit-chy708 <cse2025rohit133@iesuniversity.ac.in>
2024-05-19 23:50:04 +0530

    last commit

--- app.js
+++ app.js
@@ -21,8 +21,7 @@ const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
 
-// const dbUrl = process.env.ATLASDB_URL;
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = process.env.ATLASDB_URL;
 
 main().then(()=>{
     console.log("DB connected");
@@ -31,8 +30,7 @@ main().then(()=>{
 });
 
 async function main(){
-    // await mongoose.connect(dbUrl);
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(dbUrl);
 }
 
 app.set("view engine","ejs");
@@ -42,20 +40,20 @@ app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
-// const store = MongoStore.create({
-//     mongoUrl: dbUrl,
-//     crypto:{
-//         secret:process.env.SECRET,
-//     },
-//     touchAfter:24*3600,
-// });
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto:{
+        secret:process.env.SECRET,
+    },
+    touchAfter:24*3600,
+});
 
-// store.on("error",()=>{
-//     console.log("ERROR IN MONGO SESSION STORE",err);
-// })
+store.on("error",()=>{
+    console.log("ERROR IN MONGO SESSION STORE",err);
+})
 
 const sessionOptions = {
-    // store,
+    store,
     secret:process.env.SECRET,
     resave: false,
     saveUninitialized: true,
